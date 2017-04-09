d405657a3733166e42338679847de7408d025e8e
Stephen-Wright <sjw948@gmail.com>
2017-04-09 13:43:10 +0100

    restructured components and built html template
    removed component structure in favour of building a html template before splitting in to components. Home papge template completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,38 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { GameSearchService } from "./services/game-search.service"
 import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './pages/home/home.component';
-import { ChartsComponent } from './pages/charts/charts.component';
-import { XboxOneComponent } from './pages/xbox-one/xbox-one.component';
-import { PlaystationFourComponent } from './pages/playstation-four/playstation-four.component';
-import { NintendoSwitchComponent } from './pages/nintendo-switch/nintendo-switch.component';
-import { ReleaseDatesComponent } from './pages/release-dates/release-dates.component';
-import { FooterComponent } from './footer/footer.component';
-import { PopularGamesComponent } from './popular-games/popular-games.component';
-import { CharLimit } from "./pipes/charlimit"
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent,
-    ChartsComponent,
-    XboxOneComponent,
-    PlaystationFourComponent,
-    NintendoSwitchComponent,
-    ReleaseDatesComponent,
-    FooterComponent,
-    PopularGamesComponent,
-    CharLimit
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
   ],
-  providers: [GameSearchService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
