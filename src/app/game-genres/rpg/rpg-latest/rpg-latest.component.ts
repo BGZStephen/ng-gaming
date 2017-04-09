bbba7bd663702da51ae35e3dacec6dfc4d47ef9f
Stephen Wright <sjw948@gmail.com>
2017-04-09 21:26:06 +0100

    built game genre html / css teamplates for latest games components

--- src/app/game-genres/rpg/rpg-latest/rpg-latest.component.ts
+++ src/app/game-genres/rpg/rpg-latest/rpg-latest.component.ts
@@ -9,6 +9,17 @@ export class RpgLatestComponent implements OnInit {
 
   constructor() { }
 
+  rpgGames = {
+    primary: {imgUrl: "http://placehold.it/300x150", header: "Test Primary Header", date: "August 31, 2016", comments: "No comments", description: "This is a test description"},
+    secondary: [
+      {imgUrl: "http://placehold.it/300x150", header: "Test Secondary Header #1", date: "August 31, 2016", comments: "No comments"},
+      {imgUrl: "http://placehold.it/300x150", header: "Test Secondary Header #2", date: "August 31, 2016", comments: "No comments"},
+      {imgUrl: "http://placehold.it/300x150", header: "Test Secondary Header #3", date: "August 31, 2016", comments: "No comments"},
+    ]
+  }
+
+
+
   ngOnInit() {
   }
 
