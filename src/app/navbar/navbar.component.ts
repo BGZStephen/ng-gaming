e9877ab56507042ad4e22c36c41ec5f89a11d71f
Stephen-Wright <sjw948@gmail.com>
2017-04-11 15:43:49 +0100

    fixed menu visibility on window resize

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,18 +9,26 @@ export class NavbarComponent implements OnInit {
 
   constructor() { }
 
-  toggle: false;
+  toggle = false;
 
   ngOnInit() {
   }
 
-  toggleMenu() {
-    if(screen.width > 768) {
+  toggleMenuResize() {
+    if (screen.width < 768) {
+      this.toggleMenu("resize")
+    }
+  }
+
+  toggleMenu(resize?) {
+    if (resize && screen.width < 768) {
+      return {'visibility':'hidden', 'opacity': '0'}
+    } else if (this.toggle == false && screen.width > 768) {
       return {'visibility':'visible', 'opacity': '1'}
-    } else if (this.toggle && (screen.width < 768)) {
+    } else if (this.toggle) {
       return {'visibility':'visible', 'opacity': '1'}
     } else {
-      return {'visibility':'hidden', 'opacity': '0' }
+      return {'visibility':'hidden', 'opacity': '0'}
     }
   }
 
